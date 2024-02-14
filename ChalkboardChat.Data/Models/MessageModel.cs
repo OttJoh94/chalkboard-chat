961fa88e8bb16e8c418980f86ba962db6d52a7d8
OttJoh94 <olg.johansson@gmail.com>
2024-02-14 16:03:52 +0100

    gjorde klart allt med Db

---
+++ ChalkboardChat.Data/Models/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChalkboardChat.Data.Models
+{
+	public class MessageModel
+	{
+		[Key]
+		public int Id { get; set; }
+		public string? Username { get; set; }
+		public string? Message { get; set; }
+		public DateTime Date { get; set; } = DateTime.Now;
+	}
+}
